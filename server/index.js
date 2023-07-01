84304b441cca6ab5d70f27dca1f11968b213a251
RealYarik89 <yarok.89@gmail.com>
2023-07-01 17:01:51 +0300

    Handler error

--- server/index.js
+++ server/index.js
@@ -6,10 +6,13 @@ const models = require('./models/models')
 const cors = require("cors")
 const PORT = process.env.PORT || 5000
 const router = require("./routes/index")
+const errorHandler = require("./middleware/errorHandlingMiddleware")
 
 app.use(cors())
 app.use(express.json())
 app.use("/api", router)
+
+app.use(errorHandler)
 
 app.get("/", (req, res) => {
     res.status(200).json({message: "Work!!!"})
