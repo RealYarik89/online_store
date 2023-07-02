a7ed52f8961855a0ecfe7ad181ff2ced4fbba431
RealYarik89 <yarok.89@gmail.com>
2023-07-02 17:35:25 +0300

    gitignore

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,14 +1,29 @@
-class DeviceController{
-    async create(req, res){
-        const {name, price, typeId, brandId, info} = req.body
-        const {img} = req.files
+const uuid = require("uuid")
+const path = require("path")
+const { Device } = require("../models/models")
+const ApiError = require("../error/ApiError")
+
+class DeviceController {
+    async create(req, res, next) {
+        try {
+            const { name, price, typeId, brandId, info } = req.body
+            const { img } = req.files
+            const fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, "..", "static", fileName))
+
+            const device = await Device.create({ name, price, typeId, brandId, img: fileName })
+            return res.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+
     }
 
-    async getAll(req, res){
-        
+    async getAll(req, res) {
+
     }
-    async getOne(req, res){
-        
+    async getOne(req, res) {
+
     }
 }
 
